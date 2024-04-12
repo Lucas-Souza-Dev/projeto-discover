46b417a7e31514780301a89214be4bcc51335bc3
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2024-04-12 19:59:56 -0300

    :tada: initial commit

---
+++ projeto/script.js
@@ -0,0 +1,17 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    //pegar a tag img
+    const img = document.querySelector('#profile img');
+
+    //substituir imagem
+    if(html.classList.contains("light")){
+        img.setAttribute("src","../assets/avatar-light.png")
+        img.setAttribute("alt", "Foto do mike sorrindo de oculos escuros, usando jaqueta preta com um fundo degrade.")
+    }else{
+        img.setAttribute("src","../assets/avatar.png")
+    }
+    
+
+}
