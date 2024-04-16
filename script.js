566700efabc67430bd365e7c17865367a9dbf615
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2024-04-15 23:29:06 -0300

    linked

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function toggleMode(){
 
     //substituir imagem
     if(html.classList.contains("light")){
-        img.setAttribute("src","../assets/avatar-light.png")
+        img.setAttribute("src","./assets/avatar-light.png")
         img.setAttribute("alt", "Foto do mike sorrindo de oculos escuros, usando jaqueta preta com um fundo degrade.")
     }else{
-        img.setAttribute("src","../assets/avatar.png")
+        img.setAttribute("src","./assets/avatar.png")
     }
     
 
